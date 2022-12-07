9eec1d67393abcc5569b1cc9cccf3c5001f588b1
Thannatron <120001215+Thannatron@users.noreply.github.com>
2022-12-07 15:18:22 +0700

    Update index.php

--- index.php
+++ index.php
@@ -4,7 +4,8 @@
  </head>
  <body>
  <?php echo '<p>Hello World</p>'; ?> 
-   <?php echo '<p>My name is Itthiphol</p>'; ?> 
+   <?php echo '<p>My name is Itthiphol</p>'; ?>
+   <?php echo '<p>My name is Thannatron</p>'; ?>   
      <?php echo '<p>Update-3st</p>'; ?> 
      <?php echo '<p>Update-4st</p>'; ?> 
   <?php echo '<p>Update-555555555555st</p>'; ?> 
